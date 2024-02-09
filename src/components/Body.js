76b8ea5411eda16e28cf4bd9e4c8c06bd81b8fc2
kunal.pandey <kunal.pandey@prolitus.com>
2024-02-09 15:37:39 +0530

    use-effect and shimmer ui

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,31 @@
 import RestaurantCard from "./RestaurantCard";
 import { resList } from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
 
-  return (
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    let data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6570846&lng=77.2657627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    let res = await data.json();
+
+    data =
+      res?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+    console.log(data);
+
+    setListOfRestaurants(data);
+  };
+
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       {/* <div className="search">Search</div> */}
       <div className="filter">
